14e4cfbb3f87ac0b62bc924a7f6e9c343316e7cd
codeuronline <j.kasperski@codeur.online>
2022-05-20 09:14:56 +0200

    update code

---
+++ wp-content/plugins/my-first-plugin/my-first-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Mon premier plugin
+Plugin URI: http://wordpress.org/plugins/my-first-plugin/
+Description: bla bala car
+Author: Jeremie KASPERSKI
+Version: 0.0.1
+Author URI: http://jkasperski.free.fr/
+*/
+require_once plugin_dir_path(__FILE__) . 'includes/mfp-functions.php';
